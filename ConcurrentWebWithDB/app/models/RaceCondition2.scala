3d6404440d6632e8bb634a0ae7054798711d1b27
erlangxk <simon.xikun@gmail.com>
2015-03-06 16:04:32 +0800

    change sql to broken one, add jmeter script

--- ConcurrentWebWithDB/app/models/RaceCondition2.scala
+++ ConcurrentWebWithDB/app/models/RaceCondition2.scala
@@ -9,7 +9,7 @@ import play.api.Logger
 object RaceCondition2 extends RC {
 
   def withdraw(id: Long, delta: Int) = DB.withTransaction { implicit conn =>
-    val optb = SQL"select balance from accounts where id=$id for update".as(SqlParser.int(1).singleOpt)
+    val optb = SQL"select balance from accounts where id=$id".as(SqlParser.int(1).singleOpt)
     optb map { b =>
       if (delta > 0 && b >= delta) {
         val newBalance = b - delta
